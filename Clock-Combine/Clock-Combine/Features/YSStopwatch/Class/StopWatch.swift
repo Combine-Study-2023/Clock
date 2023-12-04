e34779db210e2724e50ada71b4e400fa6894d4d1
yeonsu <drawingbonny@gmail.com>
2023-12-04 17:52:54 +0900

    feat: 스톱워치 class 구현 (#1)

---
+++ Clock-Combine/Clock-Combine/Features/YSStopwatch/Class/StopWatch.swift
@@ -0,0 +1,18 @@
+//
+//  StopWatch.swift
+//  Clock-Combine
+//
+//  Created by yeonsu on 12/4/23.
+//
+
+import Foundation
+
+class Stopwatch: NSObject {
+  var counter: Double   // 초 경과 시간 추적
+  var timer: Timer      // 특정 간격마다 지정된 동작을 반복하거나 실행
+  
+  override init() {
+    counter = 0.0
+    timer = Timer()
+  }
+}
